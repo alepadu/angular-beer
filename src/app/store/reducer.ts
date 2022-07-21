561902536fba0260b7602e7baa24226b689b7a71
alepadu <alepadualino@gmail.com>
2022-07-21 14:43:29 +0200

    1 file modified

--- src/app/store/reducer.ts
+++ src/app/store/reducer.ts
@@ -2,7 +2,5 @@ import { createReducer } from '@ngrx/store';
 import { beersData } from '../data/beer-list';
 import { BeerState } from '../model/state';
 
-const initialBeerState: BeerState = {
-  list: beersData; 
-};
+export const initialBeerState: BeerState = { list: beersData };
 export const beerReducers: any = createReducer(initialBeerState);
